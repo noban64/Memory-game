5c4b59b26424c70744c7ebc9e87b42a313ad176f
Hamdi <32712674+noban64@users.noreply.github.com>
2025-02-26 13:56:25 +0000

    re-orders on click

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,56 +1,76 @@
 import { useEffect, useState } from "react";
 import "../styles/cards.css";
 import random from "random";
+import shuffleArray from "shuffle-array";
 
 function Cards() {
   const [characters, setCharacters] = useState([]);
-  // var characterNumbers = [random.int(1,100), 2, 3, 4, 5, 6, 7, 8];
-  var characterNumbers = new Array(8).fill().map(() => random.int(1,1025))
+
+  var characterNumbers = new Array(8).fill().map(() => {
+    random.int(1,1025)}
+  )
 
   function checkClicked(item) { 
     if (item.clicked == false) {
-      item.clicked = true; 
-      
-    }
-  }
+      return item.clicked = true; 
 
-  useEffect(() => {
+    }
+    else if (item.clicked == true) {
+      manageCharacters();
+    }
+    console.log(item)
+    shuffleOrder();
+    console.log(characters);
+  }
+  
+  function shuffleOrder(){
+    setCharacters(shuffleArray([...characters]))
+  }
+  function manageCharacters(info){ 
+    setCharacters((prevItems) => [...prevItems, info])
+  }
+  
+  useEffect(() => { 
     console.log("effect trigger");
     async function getPokemon(number) {
       try {
         const call = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
 
         const data = await call.json();
-        characters.push({
+        manageCharacters({
           id: number,
           name: data.name.toUpperCase(),
           image: `https://img.pokemondb.net/artwork/${data.name}.jpg`,
+          clicked: false,
         });
       } catch (error) { 
-        console.error(error);   
+        console.error(error);     
       }
     }
+    if (characters.length < 1) {
     characterNumbers.forEach((number) => {
       getPokemon(number); 
-    }); 
+    });
+  }
     console.log(characters);
-  }, [characters,setCharacters]); 
+  }, []);  
 
   return ( 
     // <>
-    <div id="cards"> 
+    <div id="cards">
+      {console.log("rendered!")} 
       {characters.map((character) => {
         return ( 
-        <div className="card" key={character.id} onClick={}> 
+        <div className="card" key={character.id} onClick={checkClicked} > 
           <img src={character["image"]} />
           <h2>{character["name"]}</h2>  
         </div>
-        ) 
-      })
+        )  
+      }) 
       }
-    </div>
+    </div> 
     // </>
   );
 } 
-
+  
 export default Cards;
