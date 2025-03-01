355b8521999ba843326d6debb70f0a117d157ff1
Hamdi <32712674+noban64@users.noreply.github.com>
2025-03-01 20:28:32 +0000

    final commit

--- src/components/memorygame.jsx
+++ src/components/memorygame.jsx
@@ -1,25 +1,126 @@
-import { useEffect,useState } from 'react'
-import '../styles/memorygame.css'
+import { useEffect, useState } from "react";
+import "../styles/memorygame.css";
+import "../styles/cards.css";
+import random from "random";
+import shuffleArray from "shuffle-array";
+import Cookies from "js-cookie";
 
 function Game() {
-  const [score, setScore] = useState(0)
-  const [characterOrder, setCharacterOrder] = useState(0);
-  const test = "Hey!"
-  
-  function changeScore(){
-    setScore(score +1 );
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [characters, setCharacters] = useState([]);
+  var characterNumbers = generateCharacters();
+  var gameLoss = false;
+
+  function generateCharacters() {
+    // generates unique character codes
+    let temporarySet = new Set();
+    const result = [1, 2, 3, 4, 5, 6, 7, 8].map(() => {
+      let characterNums;
+      do {
+        characterNums = random.int(1, 1025);
+      } while (temporarySet.has(characterNums));
+
+      temporarySet.add(characterNums);
+      return characterNums;
+    });
+    return result;
   }
-  function changeOrder() {
-    setCharacterOrder(); // order 
+  function checkClicked(item) {
+    if (item.clicked == false) {
+      changeScore();
+      item.clicked = true;
+    } else if (item.clicked == true) {
+      gameLoss = true;
+    }
+    shuffleOrder();
+    manageGameState();
   }
+
+  function shuffleOrder() {
+    setCharacters(shuffleArray([...characters]));
+  }
+  function manageCharacters(info) {
+    setCharacters((prevItems) => [...prevItems, info]);
+  }
+  function clearCharacters() {
+    setCharacters(new Array());
+  }
+  function changeScore() {
+    setScore(score + 1);
+  }
+  function changeHighScore(currentScore) {
+    if (currentScore > highScore) {
+      setHighScore(score);
+      Cookies.set("highScore", `${currentScore}`);
+    }
+  }
+  function parseSavedScore(score) {
+    setHighScore(score);
+  }
+
+  function manageGameState() {
+    if (gameLoss) {
+      changeHighScore(score);
+      window.location.reload(true);
+    } else if (characters.every(checkCards)) {
+      clearCharacters();
+    }
+  }
+
+  function checkCards(card) {
+    return card.clicked == true;
+  }
+
+  useEffect(() => {
+    async function getPokemon(number) {
+      try {
+        const call = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
+
+        const data = await call.json();
+        manageCharacters({
+          id: number,
+          name: data.name.toUpperCase(),
+          image: `https://img.pokemondb.net/artwork/${data.name}.jpg`,
+          image2: data.sprites.front_default,
+          clicked: false,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    if (characters.length < 1) {
+      parseSavedScore(parseInt(Cookies.get("highScore")) || 0);
+      characterNumbers.forEach((number) => {
+        getPokemon(number);
+      });
+    }
+  }, [characters]);
 
   return (
     <>
       <div>
-      <button onClick={changeScore}>score is {score}</button>
+        <button>Score: {score}</button>
+        <br />
+        <button> Highscore: {highScore}</button>
+      </div>
+
+      <div id="cards">
+        {characters.map((character) => {
+          return (
+            <div
+              className="card"
+              key={character.id}
+              onClick={() => checkClicked(character)}
+            >
+              <img src={character["image"] || character["image2"]} />
+              <h2>{character["name"]}</h2>
+            </div>
+          );
+        })}
       </div>
     </>
-  )
+  );
 }
 
-export default Game
+export default Game;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import Game from './components/memorygame.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.css";
+import Game from "./components/memorygame.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <>
     <Game />
-  </StrictMode>,
-)
+  </>
+);
